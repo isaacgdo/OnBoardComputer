2d924958f9550f060fb63504e9782c5a27834b92
talesjoabe <30672549+talesjoabe@users.noreply.github.com>
2019-10-01 13:37:57 -0300

    Add files via upload

---
+++ main.ino
@@ -0,0 +1,20 @@
+int main(){
+  DDRB &= 0b11110011; // entrada (PB2 e PB3)
+  DDRD |= 0b11110000; // saída (PD4, PD5,PD6,PD7)
+
+  PORTB |= 0b00001100; // pull up (PB2 e PB3)
+  PORTD &=0b00001111; // Saída em nível baixo (PD4 até PD7)
+  
+  while(true){
+    // configuração do ADC
+    ADMUX |= 0b01000000;// 01 ref(Vcc) ; 0 (ADC - SEM ADLAR) ; 0 (RESERVADO) ; 0000 (ADC0) 
+    ADCSRA
+    if(){
+      
+    }else{
+      
+    }
+    
+  }
+}
+
